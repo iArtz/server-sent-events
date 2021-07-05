e7a44daa8f87e50553af8cf3169c669a929b9852
iArtz <kanitphong@uservices-thailand.com>
2021-07-05 11:28:58 +0700

    added event to list on publish

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,35 @@ import cors from 'cors'
 const PORT = 3000
 const emitter = new EventEmitter()
 const app = express()
+
+const jobsList = [
+  { message: 'ABCD-MS-RY-00001' },
+  { message: 'ABCD-MS-RY-00002' },
+  { message: 'ABCD-MS-RY-00003' },
+  { message: 'ABCD-MS-RY-00004' },
+  { message: 'ABCD-MS-RY-00005' },
+  { message: 'ABCD-MS-RY-00006' },
+  { message: 'ABCD-MS-RY-00007' },
+  { message: 'ABCD-MS-RY-00008' },
+  { message: 'ABCD-MS-RY-00009' },
+  { message: 'ABCD-MS-RY-000010' },
+  { message: 'ABCD-MS-CB-00001' },
+  { message: 'ABCD-MS-CB-00002' },
+  { message: 'ABCD-MS-CB-00003' },
+  { message: 'ABCD-MS-CB-00004' },
+  { message: 'ABCD-MS-CB-00005' },
+  { message: 'ABCD-MS-SB-00001' },
+  { message: 'ABCD-MS-SB-00002' },
+  { message: 'ABCD-MS-SB-00003' },
+  { message: 'ABCD-MS-SB-00004' },
+  { message: 'ABCD-MS-SB-00005' },
+  { message: 'ABCD-MS-SB-00006' },
+  { message: 'ABCD-MS-SB-00007' },
+  { message: 'ABCD-MS-SB-00008' },
+  { message: 'ABCD-MS-SB-00009' },
+  { message: 'ABCD-MS-SB-000010' },
+]
+
 const subscribe = (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/event-stream',
@@ -34,38 +63,12 @@ const subscribe = (req, res) => {
 
 const publish = (req, res) => {
   emitter.emit('board', req.body)
+  jobsList.push({ ...req.body })
   res.json({ message: 'success' })
 }
 
 const initData = () => {
   // Data From Server
-  const jobsList = [
-    { message: 'ABCD-MS-RY-00001' },
-    { message: 'ABCD-MS-RY-00002' },
-    { message: 'ABCD-MS-RY-00003' },
-    { message: 'ABCD-MS-RY-00004' },
-    { message: 'ABCD-MS-RY-00005' },
-    { message: 'ABCD-MS-RY-00006' },
-    { message: 'ABCD-MS-RY-00007' },
-    { message: 'ABCD-MS-RY-00008' },
-    { message: 'ABCD-MS-RY-00009' },
-    { message: 'ABCD-MS-RY-000010' },
-    { message: 'ABCD-MS-CB-00001' },
-    { message: 'ABCD-MS-CB-00002' },
-    { message: 'ABCD-MS-CB-00003' },
-    { message: 'ABCD-MS-CB-00004' },
-    { message: 'ABCD-MS-CB-00005' },
-    { message: 'ABCD-MS-SB-00001' },
-    { message: 'ABCD-MS-SB-00002' },
-    { message: 'ABCD-MS-SB-00003' },
-    { message: 'ABCD-MS-SB-00004' },
-    { message: 'ABCD-MS-SB-00005' },
-    { message: 'ABCD-MS-SB-00006' },
-    { message: 'ABCD-MS-SB-00007' },
-    { message: 'ABCD-MS-SB-00008' },
-    { message: 'ABCD-MS-SB-00009' },
-    { message: 'ABCD-MS-SB-000010' },
-  ]
   jobsList.map((msg) => {
     emitter.emit('board', { message: msg.message })
   })
